3f7fe64c6435753a3e5d15d1b7ff464255211a20
anobone <91073823+anobone@users.noreply.github.com>
2023-06-10 20:00:54 +0300

    Update ins.php

--- filmsearcher/logic/ins.php
+++ filmsearcher/logic/ins.php
@@ -40,13 +40,15 @@ while ($row = $show2->fetch_assoc()){
 if(isset($_POST['insert'])) {
 
         if(!empty($_FILES['img_upload']['tmp_name'])) $img = addslashes(file_get_contents($_FILES['img_upload']['tmp_name']));
-        if(!empty($_FILES['imges_upload']['tmp_name'])) $imges = addslashes(file_get_contents($_FILES['imges_upload']['tmp_name']));
 
         $add = $con->prepare("INSERT INTO `media` (`idmedia`, `title`,`image_cover`, `release`, `rating`, `description`, `trailer`, `category_idcategory`) VALUES ( '$idadd', '$title','$img' , '$date', '$rate', '$description', '$trailer', '$kate')");
         $add->execute();
 
-        mysqli_query($con,"INSERT INTO `images`(`idimage`, `path`, `media_idmedia`) VALUES ('$idaddimg','$imges','$idadd')");
-
+         if(!empty($_FILES['imges_upload']['tmp_name'])){
+            $imges = addslashes(file_get_contents($_FILES['imges_upload']['tmp_name']));
+            mysqli_query($con,"INSERT INTO `images`(`idimage`, `path`, `media_idmedia`) VALUES ('$idaddimg','$imges','$idadd')");
+         }
+    
         foreach($genre as $i){
             $genre = $i;
             mysqli_query($con,"INSERT INTO `films_genre` (`media_idmedia`, `genres_idgenre`) VALUES ('$idadd', '$genre')");
